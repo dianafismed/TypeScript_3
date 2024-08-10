24b05e42ee7d15f26fbe3b9105a09934040e320e
Diana Avila <missernightangel@gmail.com>
2024-08-09 21:14:00 -0300

    up

---
+++ app/src/decorators/dom-injector.ts
@@ -0,0 +1,14 @@
+export function domInjector(seletor: string){
+  return function (
+    target: any,
+    propertyKey: string   // será o seletor do DOM
+  ){
+    console.log(`Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
+    const getter = function(){
+      const elemento = document.querySelector(seletor);
+      console.log(`Buscando o elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+      return elemento
+    }
+    Object.defineProperty(target, propertyKey, {get: getter});
+  }
+}
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -14,8 +14,7 @@ export abstract class View<T> {
     }
   }
 
-  @inspect()
-  @logarTempoDeExecucao(true)
+  
   public update(model: T): void {
     let template = this.template(model);
     this.elemento.innerHTML = template;
