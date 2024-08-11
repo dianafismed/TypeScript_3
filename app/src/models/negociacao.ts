5b7c1a2647b85b387942ac23edcd4c31ff4cb544
Diana Avila <missernightangel@gmail.com>
2024-08-11 19:54:20 -0300

    up

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,18 +1,14 @@
-export class Negociacao {
+import { Imprimivel } from "../utils/imprimivel.js";
+
+export class Negociacao extends Imprimivel {
   constructor(
     private _data: Date,
     public readonly quantidade: number,
     public readonly valor: number
-  ) {}
-
-  get volume(): number {
-    return this.quantidade * this.valor;
+  ) {
+    super();
   }
 
-  get data(): Date {
-    const data = new Date(this._data.getTime());
-    return data;
-  }
 
   public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
     const exp = /-/g;
@@ -21,4 +17,25 @@ export class Negociacao {
     const valor = parseFloat(valorString);
     return new Negociacao(date, quantidade, valor);
   }
+
+
+  get volume(): number {
+    return this.quantidade * this.valor;
+  }
+
+
+  get data(): Date {
+    const data = new Date(this._data.getTime());
+    return data;
+  }
+
+
+  public paraTexto(): string{
+    return `
+    Data: ${this.data}
+    Quantidade: ${this.quantidade}
+    Valor: ${this.valor}
+    `;
+  }
+
 }
--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,13 +1,21 @@
+import { Imprimivel } from '../utils/imprimivel.js';
 import { Negociacao } from './negociacao.js';
 
-export class Negociacoes {
+export class Negociacoes extends Imprimivel {
   private negociacoes: Negociacao[] = [];
+
 
   public adiciona(negociacao: Negociacao) {
     this.negociacoes.push(negociacao);
   }
 
+
   public lista(): readonly Negociacao[] {
     return this.negociacoes;
   }
+
+  public paraTexto(): string{
+    return JSON.stringify(this.negociacoes, null, 2);
+  }
+
 }
